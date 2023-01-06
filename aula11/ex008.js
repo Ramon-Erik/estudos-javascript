984881c89577b3afb0cfc1becbc444abc884c933
Ramon Erik <ramonerik29a@gmail.com>
2023-01-06 13:28:55 -0300

    Create ex008.js

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var v = 990.6
+console.log(`Velocidade: ${v}km/h`)
+
+if (v > 60) {
+    console.log(`Voce ultrapassou a velocidade! multado`)
+    if (v > 100) {
+        console.log('quer viajar no tempo?')
+    }
+}
+
+console.log(`dirija com segurança`)
