b0d3718529d84dcc78eab36e5b228e464e06379f
Ramon Erik <ramonerik29a@gmail.com>
2023-01-09 00:30:42 -0300

    Update script.js

--- aula12ex/script.js
+++ aula12ex/script.js
@@ -1,5 +1,21 @@
 var horas = new Date().getHours()
 console.log(horas)
 var pHora = document.getElementById('hora')
+var divFoto = document.getElementById('foto-horario')
 
-pHora.innerText = `<p>Agora são ${horas} horas.</p>`
+horas = 12
+
+pHora.innerText = `Agora são ${horas} horas.`
+
+if (horas >= 5 && horas < 12)  {
+    divFoto.style = 'background: #CCBA34 url(imagens/dia.jpg);'
+    document.body.style = 'background: #CCBA34'
+
+} else if (horas >= 12 && horas < 18 ) {
+    divFoto.style = 'background: #F26C09 url(imagens/tarde.jpg);'
+    document.body.style = 'background: #F26C09'
+    
+} else {
+    divFoto.style = 'background: #002150 url(imagens/noite.jpg);'
+    document.body.style = 'background: #002150'
+}
