e99a8d765cb2c562acf463664d0d493e4b438048
Ramon Erik <ramonerik29a@gmail.com>
2023-01-10 14:17:58 -0300

    Update script.js

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,21 +1,25 @@
 function analizar() {
     var idade = new Date().getFullYear() - document.getElementById('anoUsuario').value
-
-    var nomeIdade = 'idoso'
-    if (idade >= 55) {
-        nomeIdade = 'idoso'
-    } else if (idade < 55 && idade >= 20) {
-        nomeIdade = 'adulto'
-    } else if (idade < 20 && idade >= 16) {
-        nomeIdade = 'jovem'
+    
+    if (idade <= 0 || idade > 110) {
+        window.alert('ERRO! Dados Ivalidos. Tente novamente!')
     } else {
-        nomeIdade = 'bebe'
+
+        var nomeIdade = 'idoso'
+        if (idade >= 55) {
+            nomeIdade = 'idoso'
+        } else if (idade < 55 && idade >= 20) {
+            nomeIdade = 'adulto'
+        } else if (idade < 20 && idade >= 16) {
+            nomeIdade = 'jovem'
+        } else {
+            nomeIdade = 'bebe'
+        }
+
+        var sexo = document.getElementById('sexoM').checked == true? 'masculino' : 'feminino'
+
+        var areaTexto = document.getElementById('texto')
+        areaTexto.innerHTML = `<p>Aqui nós temos:<br>${nomeIdade}<br>com ${idade} anos</p>`
+        areaTexto.innerHTML += `<p><img src='imagens/${sexo}/foto-${nomeIdade}-${sexo[0]}.png' alt='foto de um ${nomeIdade} ${sexo}'></p>`
     }
-
-    var sexo = document.getElementById('sexoM').checked == true? 'masculino' : 'feminino'
-    console.log(sexo)
-
-    var areaTexto = document.getElementById('texto')
-    areaTexto.innerHTML = `<p>Aqui nós temos:<br>${nomeIdade}<br>(${idade} anos)</p>`
-    areaTexto.innerHTML += `<p><img src='imagens/${sexo}/foto-${nomeIdade}-${sexo[0]}.png' alt='foto de um ${nomeIdade} ${sexo}'></p>`
 }
