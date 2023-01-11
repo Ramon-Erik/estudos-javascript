e81ab8ef6bf1b316e2993d367f3137c4a8d70b46
Ramon Erik <ramonerik29a@gmail.com>
2023-01-11 16:32:16 -0300

    finished everything
    actually the erro was the last condition wasn't correctly closed

--- aula14ex/ex015/script.js
+++ aula14ex/ex015/script.js
@@ -9,14 +9,14 @@ function contar() {
         if (passo == 0) {
             window.alert('Passo não foi informado, considerando 1')
             passo = 1
-            var texto = document.getElementById('texto')
-
-            texto.innerText = ini
-
-            for (ini; ini <= f; ini += passo) {
-                texto.innerText += ` 👉 ${ini}`
-            }
-            texto.innerText += ' 🏁'
         }
+        var texto = document.getElementById('texto')
+
+        texto.innerText = ini
+
+        for (ini; ini <= f; ini += passo) {
+            texto.innerText += ` 👉 ${ini}`
+        }
+        texto.innerText += ' 🏁'
     }
 }
