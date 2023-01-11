3b7311c33a193ac664ece6b612f8cc842849da83
Ramon Erik <ramonerik29a@gmail.com>
2023-01-11 08:52:17 -0300

    Create script.js

---
+++ aula14ex/ex015/script.js
@@ -0,0 +1,8 @@
+function contar() {
+    var ini = document.getElementById('inicio').value
+    var f = document.getElementById('fim').value
+    var passo = document.getElementById('passo').value
+    var texto = document.getElementById('texto')
+    
+    
+}
