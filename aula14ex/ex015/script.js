b1e2fa0ae580029ce1961556099f494520caf7c8
Ramon Erik <ramonerik29a@gmail.com>
2023-01-11 16:25:23 -0300

    finished everything
    there is an erro if you use numbers below zero

--- aula14ex/ex015/script.js
+++ aula14ex/ex015/script.js
@@ -2,11 +2,21 @@ function contar() {
     var ini = Number(document.getElementById('inicio').value)
     var f = Number(document.getElementById('fim').value)
     var passo = Number(document.getElementById('passo').value)
-    var texto = document.getElementById('texto')
 
-    texto.innerText = ini
+    if (f == 0 || ini == 0) {
+        window.alert('ERRO! Verifique os dados')
+    } else {
+        if (passo == 0) {
+            window.alert('Passo não foi informado, considerando 1')
+            passo = 1
+            var texto = document.getElementById('texto')
 
-    for (ini; ini <= f; ini += passo) {
-        texto.innerText += ` -> ${ini}`
+            texto.innerText = ini
+
+            for (ini; ini <= f; ini += passo) {
+                texto.innerText += ` 👉 ${ini}`
+            }
+            texto.innerText += ' 🏁'
+        }
     }
 }
