f1931651c46dd7a83099c3c7de29f15eba9b7a50
Ramon Erik <ramonerik29a@gmail.com>
2023-01-15 13:21:57 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -12,3 +12,10 @@ console.log(`organizado ${num}`)
 for (let posicao = 0; posicao <= num.length-1; posicao++) {
     console.log(num[posicao])
 }
+console.log('//')
+for (let valor in num) {
+    console.log(num[valor])
+}
+
+console.log(`encontrando o numero 7. posição: ${num.indexOf(7)}`)
+console.log(`encontrando um numero ${num.indexOf(5)}`)
