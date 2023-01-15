087b20e09438fa1dcd985a87424d2271820affbf
Ramon Erik <ramonerik29a@gmail.com>
2023-01-14 23:00:09 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,6 +1,7 @@
 function contar() {
     var num = document.getElementById('inum').value
     var divTabuada = document.getElementById('conteudo') 
+    divTabuada.innerHTML = ''
     divTabuada.style = 'border: 1px solid black;'
     for (var c = 1; c <= 10; c++) {
         divTabuada.innerHTML += `<p>${c} x ${num} = ${num*c}</p>`
