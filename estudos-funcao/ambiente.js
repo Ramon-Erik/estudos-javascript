53b7564daa72a74ee6b8ceb3dbb5850089fae340
Ramon Erik <ramonerik29a@gmail.com>
2023-01-22 11:52:20 -0300

    Create ambiente.js

---
+++ estudos-funcao/ambiente.js
@@ -0,0 +1,12 @@
+// extrutura simples
+function digaOi() {
+    console.log('Oi')
+}
+digaOi()
+
+// usando parametros / parametros pre-definidos
+function digaNome(nome='nome não informado') {
+    console.log(`Olá! ${nome}`)
+}
+digaNome()
+digaNome('Ramon')
