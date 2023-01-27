3a9b4ce64bbb21460eae9d33cc96aa36361eea51
Ramon Erik <ramonerik29a@gmail.com>
2023-01-27 08:58:39 -0300

    Create ambiente01.js

---
+++ arrays/ambiente01.js
@@ -0,0 +1,16 @@
+//arrays são objetos
+//por serem objetos, tem metodos nativamente
+// são heterogenios (aceitam dados diferentes)
+// mutavel
+
+// forma literal
+let arr = []
+// ou
+let array = [1, 2, 3]
+// forma onstrutora
+let arrayConstructed = new Array()
+// ou
+let arrayConstr = new Array(10) //array de 10 posições
+
+const arrDenso = [1,2,3]
+const arrEsparso = [1,,,,,6,7] // arrEsparso[0] == 1 | arrEsparso[1] == undefined
