fc42c10ddd2ef9bff984d28b495c0264a27d55cc
Ramon Erik <ramonerik29a@gmail.com>
2023-01-27 14:00:13 -0300

    class

---
+++ hcodebr/basico-2/celular.js
@@ -0,0 +1,13 @@
+class celular {
+    constructor() {
+        this.cor = 'preto'
+        this.nome = 'Xime12xame009981'
+    }
+    ligar() {
+        return 'ligando'
+    }
+}
+
+let cel = new celular()
+console.log(cel.cor)
+console.log(cel.ligar())
