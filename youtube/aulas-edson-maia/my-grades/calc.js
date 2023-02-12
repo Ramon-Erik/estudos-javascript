64d05a239aad3fd136c6a75e5667fc475c80ce7c
Ramon Erik <ramonerik29a@gmail.com>
2023-02-11 22:11:52 -0300

    first part of the project

--- youtube/aulas-edson-maia/my-grades/calc.js
+++ youtube/aulas-edson-maia/my-grades/calc.js
@@ -1,10 +1,21 @@
 let btnCalculate = document.querySelector('#calc')
+let btnReset = document.querySelector('#resetCamps')
+let divMedia = document.querySelector('#media')
+
+function showMedia(grades) {
+    divMedia.innerHTML = `
+    <p>Your media: ${((grades[0]+grades[1]+grades[2])/3).toFixed(1)}</p>
+    `
+}
+
+btnReset.addEventListener('click', () => {
+    divMedia.innerHTML = ''
+})
 
 btnCalculate.addEventListener('click', () => {
     let grades = []
-    for (let c = 0; c <= 3; c++) {
-        grades.push(...grades, document.querySelectorAll('.numb')[c])
-    }
-    console.log(grades)
-}
-)
+    document.querySelectorAll('.numb').forEach(element => {
+        grades.push(Number(element.value))
+    });
+    showMedia(grades)
+})
