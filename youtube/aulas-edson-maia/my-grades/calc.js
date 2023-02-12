2c8472c1327e8ce3069a9d007a02da1023fb07f8
Ramon Erik <ramonerik29a@gmail.com>
2023-02-11 22:44:21 -0300

    Update calc.js

--- youtube/aulas-edson-maia/my-grades/calc.js
+++ youtube/aulas-edson-maia/my-grades/calc.js
@@ -3,16 +3,32 @@ let btnReset = document.querySelector('#resetCamps')
 let divMedia = document.querySelector('#media')
 
 function showMedia(grades) {
+    grades = ((grades[0]+grades[1]+grades[2])/3).toFixed(1)
+    let situation
+    if (grades >= 7) {
+        situation = 'Approved'
+        divMedia.style.backgroundColor = 'green'
+    } else if (grades >= 5) {
+        situation = 'Recuperation'
+        divMedia.style.backgroundColor = 'yellow'
+    } else {
+        situation = 'Repproved'
+        divMedia.style.backgroundColor = 'red'
+    }
+
     divMedia.innerHTML = `
-    <p>Your media: ${((grades[0]+grades[1]+grades[2])/3).toFixed(1)}</p>
+    <p>Your media: ${grades}<br>
+    ${situation}
+    </p>
     `
 }
 
 btnReset.addEventListener('click', () => {
-    divMedia.innerHTML = ''
+    divMedia.style.display = 'none'
 })
 
 btnCalculate.addEventListener('click', () => {
+    divMedia.style.display = 'block'
     let grades = []
     document.querySelectorAll('.numb').forEach(element => {
         grades.push(Number(element.value))
