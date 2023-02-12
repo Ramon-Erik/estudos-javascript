a4986d565054c7b63d780a1445ff43137562bf97
Ramon Erik <ramonerik29a@gmail.com>
2023-02-12 19:42:06 -0300

    dice roller v2

--- youtube/aulas-edson-maia/virtual-dice-roller/roll.js
+++ youtube/aulas-edson-maia/virtual-dice-roller/roll.js
@@ -1,5 +1,23 @@
-// let number = document.querySelector('#number')
+let number = document.querySelector('#number')
+let btnR = document.querySelector('#rollerBtn')
+let img = document.querySelector('.dice>img')
 
-// setInterval(() => {
-//     number.innerText = (Math.random()).toFixed(1)*10
-// }, 3000);
+function playSound() {
+    let audio = new Audio('assets/songs/dado-rolando.mp3')
+    audio.play()
+}
+
+function roll() {
+    btnR.style.display = 'none'
+    let n = parseInt(Math.random()*7)
+    if (n == 0) {
+        n++
+    } else if (n > 6) {
+        n--
+    }
+    img.src = `assets/dice/${n}.png`
+    number.innerText = n
+    setTimeout(() => {
+    btnR.style.display = 'inline-block'
+    },2000)
+}
