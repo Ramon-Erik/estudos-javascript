923d3b6d9759a284065a237ff94b5514232e9448
Ramon Erik <ramonerik29a@gmail.com>
2023-02-19 05:35:43 -0300

    temperature converter done

--- youtube/aulas-edson-maia/temp-converter/temperature.js
+++ youtube/aulas-edson-maia/temp-converter/temperature.js
@@ -1,4 +1,6 @@
 let textAreas = document.querySelectorAll('textarea')
+let inputTemps = document.querySelectorAll('input[type=range]')
+
 let inputC = document.querySelector('#iRangeC')
 
 refreshC(inputC)
@@ -19,4 +21,10 @@ function refreshK(input) {
     textAreas[2].value = input.value
     textAreas[0].value = (Number(input.value) - 273.1).toFixed(1)
     textAreas[1].value = ((Number(input.value) - 273.1) * (9/5) + 32).toFixed(1)
+}
+
+function clearAll() {
+    textAreas[0].value = inputTemps[0].value = 0
+    textAreas[1].value = inputTemps[1].value = 32
+    textAreas[2].value = inputTemps[2].value = 273.1
 }
