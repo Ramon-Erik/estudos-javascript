83bcba6093721c07a6275ba9022f3e73beaf0d7b
Ramon Erik <ramonerik29a@gmail.com>
2023-02-19 21:29:28 -0300

    starting js

---
+++ youtube/aulas-edson-maia/quiz-site/quiz.js
@@ -0,0 +1,18 @@
+let questions = [
+    [
+        2,
+        'Who is the father of..?',
+        'Alan Turing',
+        'Rasmus Lerdof',
+        'Brendan Eich'
+    ]
+]
+
+let nQuest   = document.querySelector('.num-quest')
+let quest    = document.querySelector('.question > h4')
+let infoPage = document.querySelector('.info > span')
+let options  = document.querySelectorAll('.option')
+
+function verify() {
+    
+}
