ddbe598f52c01715cc2bc9340f163d1f8f76fa30
Ramon Erik <ramonerik29a@gmail.com>
2023-02-21 13:39:16 -0300

    function verify

--- youtube/aulas-edson-maia/quiz-site/quiz.js
+++ youtube/aulas-edson-maia/quiz-site/quiz.js
@@ -1,18 +1,42 @@
 let questions = [
     [
-        2,
+        'Wich language is from Guido Van Rossum?',
+        'C++',
+        'JavaScript',
+        'Python.'
+    ],
+    [
         'Who is the father of..?',
-        'Alan Turing',
+        'Alan Turing.',
         'Rasmus Lerdof',
         'Brendan Eich'
     ]
 ]
 
-let nQuest   = document.querySelector('.num-quest')
-let quest    = document.querySelector('.question > h4')
-let infoPage = document.querySelector('.info > span')
-let options  = document.querySelectorAll('.option')
+let nQuest
+let quest = document.querySelector('.question > h4')
+let infoPage = document.querySelector('.info > p')
+let options = document.querySelectorAll('.option')
 
-function verify() {
-    
+function next() {
+    console.log('next')
+}
+
+function verify(btn) {
+    nQuest = Number(document.querySelector('.num-quest').innerText) - 1
+    for (let c = 1; c < 4; c++) {
+        console.log(btn.innerText+'.' + '  ' + questions[nQuest][c])
+        if (btn.innerText+'.' == questions[nQuest][c]) {
+            console.log('ok')
+            btn.style.backgroundColor = 'green'
+            next()
+            break
+        } else {
+            console.log('not ok')
+            btn.style.backgroundColor = 'red'
+            setTimeout(() => {
+                btn.style.backgroundColor = 'transparent'
+            },1200)
+        }
+    }
 }
