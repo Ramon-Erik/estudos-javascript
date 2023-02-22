9ca406b3a0106befc669684a0197d9b8f1339db8
Ramon Erik <ramonerik29a@gmail.com>
2023-02-22 14:52:07 -0300

    Update quiz.js

--- youtube/aulas-edson-maia/quiz-site/quiz.js
+++ youtube/aulas-edson-maia/quiz-site/quiz.js
@@ -1,6 +1,6 @@
-// let infoPage = document.querySelector('.info > span')
-// let quest = document.querySelector('.question > h4')
-// let options = document.querySelectorAll('.option')
+let nQuest = document.querySelector('.num-quest')
+let score = document.querySelector('.points > p')
+let totPonts = 0
 let questions = [
     [ //q1
         'Wich language is from Guido Van Rossum?',
@@ -16,25 +16,34 @@ let questions = [
     ]
 ]
 
-let nQuest
-let points = document.querySelector('.points > p')
-
-function next() {
-    console.log('next')
+function next(index) {
+    let quest = document.querySelector('.question > h4')
+    let options = document.querySelectorAll('.option')
+    let footerQuest = document.querySelector('.info > span')
+    
+    options.forEach((element, i) => {
+        // console.log(element, i, index)
+        element.style.backgroundColor = 'transparent'
+        element.innerText = questions[index][i]
+    })
+    quest.innerText = questions[index][0]
+    nQuest.innerText = ++index
+    footerQuest.innerText = `Question ${index} of 5`
 }
 
-function points() {
-    return
+function fPoints(points) {
+    totPonts += points
+    score.innerText = 'points: ' + totPonts
 }
 
 function verify(btn) {
-    nQuest = Number(document.querySelector('.num-quest').innerText) - 1
-    if (btn.innerText + '.' == questions[nQuest][1]) {
+    if (btn.innerText + '.' == questions[Number(nQuest.innerText)-1][1]) {
         btn.style.backgroundColor = 'green'
-        console.log(Number(points.innerText))
-        next()
+        fPoints(10)
+        next(Number(nQuest.innerText))
     } else {
         btn.style.backgroundColor = 'red'
+        fPoints(-10)
         setTimeout(() => {
             btn.style.backgroundColor = 'transparent'
         }, 1200)
