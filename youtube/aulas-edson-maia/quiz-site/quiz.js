0e4dbd4b65d226f771b108e456591d82544ad915
Ramon Erik <ramonerik29a@gmail.com>
2023-02-27 00:21:25 -0300

    all questions done and next function almost done

--- youtube/aulas-edson-maia/quiz-site/quiz.js
+++ youtube/aulas-edson-maia/quiz-site/quiz.js
@@ -4,15 +4,33 @@ let totPonts = 0
 let questions = [
     [ //q1
         'Wich language is from Guido Van Rossum?',
-        'Python.',
+        'Python,',
         'C++',
         'JavaScript'
     ],
     [ //q2
-        'Who is the father of..?',
-        'Alan Turing.',
+        'Who is the father of technology?',
+        'Alan Turing,',
         'Rasmus Lerdof',
         'Brendan Eich'
+    ],
+    [ //q3
+        'Complete: PHP is in ... of web,',
+        '78.9%,',
+        '79.4%',
+        '72.1%'
+    ],
+    [ //q4
+        'What does I.T. stand for?',
+        'Information technology,',
+        'Insecurity and Tired',
+        'Internet Technology'
+    ],
+    [ //q5
+        'What do the IT companies Microsoft, Apple, HP, Google all have in common?',
+        'They were all started in garages,',
+        'They were bought from Meta',
+        'They are ilusrated books'
     ]
 ]
 
@@ -20,32 +38,41 @@ function next(index) {
     let quest = document.querySelector('.question > h4')
     let options = document.querySelectorAll('.option')
     let footerQuest = document.querySelector('.info > span')
-    
-    options.forEach((element, i) => {
-        // console.log(element, i, index)
-        element.style.backgroundColor = 'transparent'
-        element.innerText = questions[index][i]
-    })
-    quest.innerText = questions[index][0]
-    nQuest.innerText = ++index
-    footerQuest.innerText = `Question ${index} of 5`
+    if (index < 5) { // displaying questions
+        // changing the options text
+        options.forEach((element, i) => {
+            element.classList.remove('color-g')
+            element.innerText = questions[index][i + 1].replace(',', '')
+        })
+        quest.innerText = questions[index][0]
+        nQuest.innerText = ++index
+        footerQuest.innerText = `Question ${index} of 5`
+    } else { // displaying the final result
+        options.forEach(element => {
+            element.style.display = 'block'
+            console.log(element)
+        })
+    }
 }
 
-function fPoints(points) {
+function fPoints(points) { // function to change the score
     totPonts += points
     score.innerText = 'points: ' + totPonts
 }
 
-function verify(btn) {
-    if (btn.innerText + '.' == questions[Number(nQuest.innerText)-1][1]) {
-        btn.style.backgroundColor = 'green'
-        fPoints(10)
-        next(Number(nQuest.innerText))
+function verify(btn) { // verifying if the answer is correct
+    if (btn.innerText + ',' == questions[Number(nQuest.innerText) - 1][1]) {
+        // the nummber in nQuest - 1 is th current question in the array and [1] is aways the correct awnser
+        btn.classList.add('color-g')
+        setTimeout(() => {
+            fPoints(10)
+            next(Number(nQuest.innerText))
+        }, 1300)
     } else {
-        btn.style.backgroundColor = 'red'
+        btn.classList.add('color-r')
         fPoints(-10)
         setTimeout(() => {
-            btn.style.backgroundColor = 'transparent'
+            btn.classList.remove('color-r')
         }, 1200)
     }
 }
