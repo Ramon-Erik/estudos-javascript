2465abe1fd98a4a426d13b3681c7a4b20ffb6509
Ramon Erik <ramonerik29a@gmail.com>
2023-03-05 12:12:35 -0300

    last changes

--- youtube/aulas-edson-maia/quiz-site/quiz.js
+++ youtube/aulas-edson-maia/quiz-site/quiz.js
@@ -1,34 +1,35 @@
+let footerQuest = document.querySelector('.info > span')
 let nQuest = document.querySelector('.num-quest')
 let score = document.querySelector('.points > p')
 let totPonts = 0
 let questions = [
     [ //q1
         'Wich language is from Guido Van Rossum?',
-        'Python,',
+        'Python!',
         'C++',
         'JavaScript'
     ],
     [ //q2
         'Who is the father of technology?',
-        'Alan Turing,',
+        'Alan Turing!',
         'Rasmus Lerdof',
         'Brendan Eich'
     ],
     [ //q3
         'Complete: PHP is in ... of web,',
-        '78.9%,',
+        '78.9%!',
         '79.4%',
         '72.1%'
     ],
     [ //q4
         'What does I.T. stand for?',
-        'Information technology,',
+        'Information technology!',
         'Insecurity and Tired',
         'Internet Technology'
     ],
     [ //q5
         'What do the IT companies Microsoft, Apple, HP, Google all have in common?',
-        'They were all started in garages,',
+        'They were all started in garages!',
         'They were bought from Meta',
         'They are ilusrated books'
     ]
@@ -46,18 +47,42 @@ function createRandomIndex(arrayLength) {
     return arr
 }
 
+// this function reload the page
+function restart() {
+    window.location.reload()
+}
+
+// this function shows up when you answer the five questions
+function showScore() {
+    let lastScreen = document.querySelector('.questions')
+    score.style.display = 'none'
+    footerQuest.style.display = 'none'
+    lastScreen.innerHTML = `
+        <h3>Nice one, You got to the end of the quiz</h3>
+        <p>Your score: ${totPonts}</p>
+    `
+    let msg
+    if (totPonts >= 30) {
+        msg = '<p>That\'s a very good score!</p>'
+    } else {
+        msg = '<p>That\'s a very bad score dude!</p>'
+    }
+    lastScreen.innerHTML += `
+        ${msg}
+        <button type="button" onclick="restart()">Restart Quiz</button>
+    `
+}   
+
 function next(index) {
     let quest = document.querySelector('.question > h4')
     let options = document.querySelectorAll('.option')
-    let footerQuest = document.querySelector('.info > span')
-
     // displaying questions
     if (index < 5) {
         // changing the options text randomly
         let randomIndex = createRandomIndex(3)
         options.forEach((element, i) => {
             element.classList.remove('color-g')
-            element.innerText = questions[index][randomIndex[i]].replace(',', '')
+            element.innerText = questions[index][randomIndex[i]].replace('!', '')
         })
         // changing the number and text of the question and the .info
         quest.innerText = questions[index][0]
@@ -66,7 +91,7 @@ function next(index) {
         // displaying the final result
     } else {
         // displaying the final result
-        let final
+        showScore()
     }
 }
 
@@ -78,7 +103,7 @@ function fPoints(points) {
 
 // verifying if the answer is correct
 function verify(btn) {
-    if (btn.innerText + ',' == questions[Number(nQuest.innerText) - 1][1]) {
+    if (btn.innerText + '!' == questions[Number(nQuest.innerText) - 1][1]) {
         // the nummber in nQuest - 1 is th current question in the array and [1] is aways the correct awnser
         btn.classList.add('color-g')
         setTimeout(() => {
