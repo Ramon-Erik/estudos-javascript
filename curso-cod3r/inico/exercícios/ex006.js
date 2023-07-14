ec26245898ee713a4b37f81bd304beac6dad28c8
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2023-07-14 06:59:34 -0300

    Update ex006.js

--- curso-cod3r/inico/exercícios/ex006.js
+++ curso-cod3r/inico/exercícios/ex006.js
@@ -1,3 +1,14 @@
 /* enunciado ex006:
     Elabore duas funções que recebem três parâmetros: capital inicial, taxa de juros e tempo de aplicação. A primeira função retornará o montante da aplicação financeira sob o regime de juros simples e a segunda retornará o valor da aplicação sob o regime de juros compostos.
 */
+
+function calcularJuroSimples(c,i,t) {
+    return c + (c*i*t)
+}
+
+function calcularJuroComposto(c,i,t) {
+    return c * (1+i)**t
+}
+
+console.log(calcularJuroSimples(100, 2, 10/100))
+console.log(calcularJuroComposto(100, 2, 10/100))
