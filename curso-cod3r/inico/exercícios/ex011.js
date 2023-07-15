4efa07278cd1ba479c2c5389ee22382a4140ee2b
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2023-07-15 00:48:29 -0300

    Create ex011.js

---
+++ curso-cod3r/inico/exercícios/ex011.js
@@ -0,0 +1,25 @@
+/* enunciado ex011:
+As regras para o cálculo dos anos bissextos são as seguintes:
+De 4 em 4 anos é ano bissexto;
+De 100 em 100 anos não é ano bissexto;
+De 400 em 400 anos é ano bissexto;
+Prevalecem as últimas regras sobre as primeiras.
+Partindo daí elabore uma função que recebe um ano e calcula se ele é ano bissexto, imprimindo no console a
+mensagem e retornando true ou false. */
+
+function eBisexto(ano) {
+    if (ano % 400 === 0) {
+        if (ano % 4 === 0) {
+            console.log(ano, 'é bisexto')
+            return true
+        }
+    }
+    console.log(ano, 'não é bisexto')
+    return false
+}
+
+eBisexto(2400)
+eBisexto(2096)
+eBisexto(2016)
+eBisexto(2024)
+for (let i = 0; i  < 10; i++) {eBisexto(Math.random().toFixed(4) * 10000)}
