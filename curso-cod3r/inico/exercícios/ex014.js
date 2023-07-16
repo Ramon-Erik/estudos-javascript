986aaf9d84597436390c08aabfa748ee1d9a1e4e
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2023-07-15 23:04:16 -0300

    Create ex014.js

---
+++ curso-cod3r/inico/exercícios/ex014.js
@@ -0,0 +1,21 @@
+/* enunciado ex014:
+    Crie uma estrutura condicional switch que receba uma string com o nome de uma fruta e que possua três casos: Caso maçã, retorne no console: “Não vendemos esta fruta aqui”. Caso kiwi, retorne: “Estamos com escassez de kiwis”. Caso melancia, retorne: “Aqui está, são 3 reais o quilo”. Teste com estas três opções .Crie também um default, que retornará uma mensagem de erro no console.
+*/
+
+function frutasNoComercio(fruta) {
+    switch (fruta) {
+        case 'maçã':
+            return 'Não vendemos esta fruta aqui'
+        case 'kiwi':
+            return 'Estamos com escassez de kiwis'
+        case 'melancia':
+            return 'Aqui está, são 3 reais o quilo'
+        default:
+            return 'inválido'
+    }
+}
+
+console.log(frutasNoComercio('maçã'))
+console.log(frutasNoComercio('kiwi'))
+console.log(frutasNoComercio('melancia'))
+console.log(frutasNoComercio('alcatra'))
