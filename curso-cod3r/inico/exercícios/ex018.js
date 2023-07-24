3b6f6fc36102dc2e11783b5046dd09ee9b9dde71
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2023-07-24 05:24:05 -0300

    Create ex018.js

---
+++ curso-cod3r/inico/exercícios/ex018.js
@@ -0,0 +1,45 @@
+/* enunciado ex018:
+    Faça um programa que leia um número entre 0 e 10, e escreva este número por extenso. Use o comando switch. Crie um case default que escreva ‘Número fora do intervalo.’
+*/
+
+function numeroPorExtenso(num) {
+    switch (num) {
+        case 0:
+            return 'zero'
+        case 1:
+            return 'um'
+        case 2:
+            return 'dois'
+        case 3:
+            return 'três'
+        case 4:
+            return 'quatro'
+        case 5:
+            return 'cinco'
+        case 6:
+            return 'seis'
+        case 7:
+            return 'sete'
+        case 8:
+            return 'oito'
+        case 9:
+            return 'nove'
+        case 10:
+            return 'dez'
+        default:
+            return 'Valor inválido'
+    }
+}
+
+console.log(numeroPorExtenso(0))
+console.log(numeroPorExtenso(1))
+console.log(numeroPorExtenso(2))
+console.log(numeroPorExtenso(3))
+console.log(numeroPorExtenso(4))
+console.log(numeroPorExtenso(5))
+console.log(numeroPorExtenso(6))
+console.log(numeroPorExtenso(7))
+console.log(numeroPorExtenso(8))
+console.log(numeroPorExtenso(9))
+console.log(numeroPorExtenso(10))
+console.log(numeroPorExtenso(11))
