f791851a2336cf54920220b9ee4bf2307ec8634c
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2023-07-24 05:24:47 -0300

    Create ex020.js

---
+++ curso-cod3r/inico/exercícios/ex020.js
@@ -0,0 +1,37 @@
+/* enunciado ex020
+    Crie um programa para informar quais e quantas notas são necessárias para entregar o mínimo de cédulas para um determinado valor informado pelo usuário considerando notas de R$ 100, R$ 50, R$ 10 e R$ 5 e R$ 1. Seu programa deve mostrar apenas as notas utilizadas. Por exemplo, a solicitar R$18, o programa deve informar apenas a seguinte informação (note que não foram exibidas informações sobre as demais cédulas): 1 nota(s) de R$ 10. 1 nota(s) de R$ 5. 3 nota(s) de R$ 1.
+*/
+
+function entregarCedulas(dinheiro) {
+    const quantidadeCedulas = [100, 50, 10, 5, 1]
+    const darCedulas = []
+    while (dinheiro > 0) {
+        if (dinheiro % 100 === 0) {
+            darCedulas.push(100)
+            dinheiro -= 100
+        } else if (dinheiro % 50 === 0) {
+            darCedulas.push(50)
+            dinheiro -= 50
+        } else if (dinheiro % 10 === 0) {
+            darCedulas.push(10)
+            dinheiro -= 10
+        } else if (dinheiro % 5 === 0) {
+            darCedulas.push(5)
+            dinheiro -= 5
+        } else {
+            darCedulas.push(1)
+            dinheiro -= 1
+        }
+    }
+    let contagem = darCedulas.reduce(function (acumulador, elemento) {
+        if (acumulador[elemento]) {
+            acumulador[elemento]++;
+        } else {
+            acumulador[elemento] = 1;
+        }
+        return acumulador;
+    }, {});
+    console.log(contagem)
+}
+
+entregarCedulas(143)
