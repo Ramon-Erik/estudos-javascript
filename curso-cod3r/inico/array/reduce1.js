312aff83612121efc564a989f63a834f2a975384
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2023-08-01 10:32:07 -0300

    assisti 3

--- curso-cod3r/inico/array/reduce1.js
+++ curso-cod3r/inico/array/reduce1.js
@@ -1,8 +1,8 @@
 const alunos = [
     { nome: 'João', nota: 7.3, bolsista: false },
-    { nome: 'Maria', nota: 9.2, bolsista: true },
+    { nome: 'Maria Aparecida', nota: 9.2, bolsista: true },
     { nome: 'Pedro', nota: 9.8, bolsista: false },
-    { nome: 'Ana', nota: 8.7, bolsista: true }
+    { nome: 'Maria de Fátima', nota: 8.7, bolsista: true }
 ]
 
 console.log(alunos.map(a => a.nota))
